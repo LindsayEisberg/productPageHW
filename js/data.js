ce64213caa733ed4473548b4c0793b9d6810fb12
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-27 13:17:04 -0500

    adding styling to page

--- js/data.js
+++ js/data.js
@@ -1,47 +1,47 @@
 var products = [
 
   {
-  title: 'Product 1',
-  image: 'http://fillmurray.com/300/400',
+  title: 'American Football',
+  image: '../photos/americanFootball.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   price: '$9.99'
 },
 
 {
-  title: 'Product 2',
-  image: 'http://fillmurray.com/300/400',
+  title: 'Billy Joel',
+  image: '../photos/billyjoel.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   price: '$20.00'
 },
 
 
 {
-  title: 'Product 3',
-  image: 'http://fillmurray.com/300/400',
+  title: 'Fleetwood Mac',
+  image: '../photos/fleetwood.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   price: '$110.95'
 },
 
 
 {
-  title: 'Product 4',
-  image: 'http://fillmurray.com/300/400',
+  title: 'Frank Turner',
+  image: '../photos/frankTurner.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   price: '$40.00'
 },
 
 
 {
-  title: 'Product 5',
-  image: 'http://fillmurray.com/300/400',
+  title: 'The Gaslight Anthem',
+  image: '../photos/gaslight.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   price: '$19.99'
 },
 
 
 {
-  title: 'Product 6',
-  image: 'http://fillmurray.com/300/400',
+  title: 'Lucero',
+  image: '../photos/NobodysDarlings.jpg',
   description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   price: '$1.99'
 },
--- js/templates.js
+++ js/templates.js
@@ -3,11 +3,13 @@ var templates = {};
 
 templates.product = [
   '<article>',
+  '<div class=wrapper>',
   '<h2><%= title %></h2>',
-  '<img src=<%= image %>/>',
+  '<img src=<%= image %> />',
   '<h3><%= "Product Description" %></h3>',
   '<p><%= description %></p>',
   '<h3><%= price %></h3>',
+  '</div>',
   '</article>'
 
 ].join("");
