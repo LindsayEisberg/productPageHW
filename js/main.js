64b2178cba1087afe7ae61cd0d59ef4efe7dd5e7
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-01-27 18:23:13 -0500

    CRUD

--- js/main.js
+++ js/main.js
@@ -2,39 +2,93 @@ var productPage = {
   init:function() {
     productPage.initStyling();
     productPage.initEvents();
-    productPage.allProducts(products);
+    productPage.renderAllProducts(products);
   },
 
   initStyling: function(){
     console.log('styling loaded correctly');
   },
    initEvents: function(){
+     $('form').on('submit', function(event) {
+       event.preventDefault();
+       productPage.createProduct();
+     });
+
+     $('form').on('updateProduct', function(event){
+       event.preventDefault();
+       productPage.updateProduct();
+     });
+
+     $('section').on('click', '.deleteProduct', productPage.deleteProduct);
+     $('section').on('click', '.updateProduct', productPage.updateProduct);
+
+
      console.log('events loaded correctly');
 
    },
 
+   createProduct: function (){
+     var newProduct = {
+       title: $('input[name="title"]').val(), //title goes here
+       image: $('input[name="image"]').val(), //image url
+       description: $('textarea[name="description"]').val(),
+       price: $('input[name="price"]').val(),
+     };
+
+     products.push(newProduct);
+     productPage.renderProduct(newProduct, products.indexOf(newProduct));
+
+     $('input').val('');
+     $('textarea').val('');
+   },
+
+
+   updateProduct: function (event){
+     event.preventDefault();
+     var title = $(this).closest('article').find('h2').text(); //title goes here
+     var image = $(this).closest('article').find('img').attr('src'); //image url
+     var description = $(this).closest('article').find('p').text();
+     var price = $(this).closest('article').find('h3').last().text();
+
+
+
+    $(this).closest('.wrapper').replaceWith(
+      '<form>' +
+      '<input type=text value=' + title + ' >'
+      + '<input type=url value=' + image + ' >'
+      + '<textarea name=>' + description + '</textarea>'
+      + '<input type=text value=' + price + ' >'
+      + '<button class="updateProduct">' + "Update Product" + '</button>'
+      + '</form>'
+    );
+
+
+   var thisIndex = $(this).closest('article').data('index');
+   products.splice(thisIndex, 1, updateProduct);
+   productPage.renderAllProducts(products);
+
+ },
+   deleteProduct: function (event){
+     var productIndex = $(this).closest('article').data('index');
+     products.splice(productIndex, 1);
+
+     $(this).closest('article').remove();
+
+   },
 //adding functions to the single products
 
-  addProduct: function (product, index, array) {
-    // $('section').append(
-    //   '<article>' +
-    //   '<h2>' + product.title + '</h2>'
-    //   + '<img src=' + product.image + ' />'
-    //   + '<h3>' + "Product Description" + '</h3>'
-    //   + '<p>' + product.description + '</p>'
-    //   + '<h3>' + product.price + '</h3>'
-    //   + '</article>'
-    // );
+  renderProduct: function (product, index, array) {
+    product.idx = index;
     var compiled = _.template(templates.product);
-    $('section').append(compiled(product));
+    $('section').prepend(compiled(product));
 
   },
 
 
 //creating multiple posts using the above iteration
 
-allProducts: function(productInfo) {
-  productInfo.forEach(productPage.addProduct);
+renderAllProducts: function(productInfo) {
+  _.each(productInfo, productPage.renderProduct);
 
 }
 
--- js/templates.js
+++ js/templates.js
@@ -2,13 +2,15 @@
 var templates = {};
 
 templates.product = [
-  '<article>',
+  '<article data-index="<%= idx %>">',
   '<div class=wrapper>',
   '<h2><%= title %></h2>',
   '<img src=<%= image %> />',
   '<h3><%= "Product Description" %></h3>',
   '<p><%= description %></p>',
   '<h3><%= price %></h3>',
+  '<p><button class="deleteProduct">Delete Product</button>',
+  '<button class="updateProduct">Update Product</button></p>',
   '</div>',
   '</article>'
 
